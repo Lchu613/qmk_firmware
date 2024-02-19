7a5bd1d8991c2ec8f208fe92aeffa9eb3054d6ce
Landon Chu <lchu059@gmail.com>
2024-02-19 00:02:13 -0500

    Custom layout for Skyloong GK61 Pro 48

---
+++ keyboards/skyloong/gk61/pro_48/keymaps/Lchu613/keymap.c
@@ -0,0 +1,68 @@
+// Copyright 2021 JZ-Skyloong (@JZ-Skyloong)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+enum layer_names {
+    _BASE,
+    _FN1,
+    _FN2,
+    _FN3
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
+     * │Esc│ 1 │ 2 │ 3 │ 4 │ 5 │ 6 │ 7 │ 8 │ 9 │ 0 │ - │ = │Bsp│
+     * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
+     * │Tab│ Q │ W │ E | R │ T │ Y │ U │ I │ O │ P │ [ │ ] │ \ │
+     * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
+     * │Cap│ A │ S │ D │ F │ G │ H │ J │ K │ L │ ; │ ' │   |Ent│
+     * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
+     * │Sft│ Z │ X │ C │ V │ B │ N │ M │ , │ . │ / │   |Mo3|   │
+     * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
+     * │Ctl│GUI│Alt│   │Spc│Spc│Mut│   |Mo1|Alt│App│Ctl│   │Mo2|
+     * └───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘
+     */
+    [_BASE] = LAYOUT_all(
+         KC_ESC,     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,        KC_6,     KC_7,        KC_8,      KC_9,        KC_0,     KC_MINS,    KC_EQL,         KC_BSPC,
+         KC_TAB,     KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,        KC_Y,     KC_U,        KC_I,      KC_O,        KC_P,     KC_LBRC,   KC_RBRC,         KC_BSLS,
+        KC_LCTL,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,        KC_H,     KC_J,        KC_K,      KC_L,     KC_SCLN,     KC_QUOT,                     KC_ENT,
+        KC_LSFT,     KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,        KC_N,     KC_M,     KC_COMM,    KC_DOT,     KC_SLSH,                MO(_FN3),
+        KC_LCTL,  KC_LGUI,  KC_LALT,             KC_SPC,   KC_SPC,     KC_MUTE,              MO(_FN1),   KC_RALT,      KC_APP,     KC_RCTL,              MO(_FN2)
+    ),
+
+     [_FN1] = LAYOUT_all(
+        KC_GRV,     KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,       KC_F6,    KC_F7,       KC_F8,     KC_F9,      KC_F10,      KC_F11,    KC_F12,          KC_DEL,
+        KC_CAPS,  KC_BTN3,  KC_HOME,  KC_MS_U,   KC_END,  KC_BTN1,     KC_PGUP,  KC_HOME,      KC_INS,    KC_END,      KC_DEL,     KC_BSPC,   _______,         _______,
+        _______,  KC_BTN4,  KC_MS_L,  KC_MS_D,  KC_MS_R,  KC_BTN2,     KC_LEFT,  KC_DOWN,       KC_UP,   KC_RGHT,     _______,     _______,                    _______,
+        _______,  KC_BTN5,  KC_MPRV,  KC_MPLY,  KC_MNXT,  _______,     KC_PGDN,  KC_VOLD,     KC_VOLU,   KC_MUTE,     _______,                _______,
+        _______,  _______,  _______,            _______,  _______,     _______,               _______,   _______,     _______,     _______,              _______
+    ),
+
+    [_FN2] =  LAYOUT_all(
+        KC_GRV ,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,       KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_DEL,
+        KC_CAPS,  _______,  _______,  _______,  RGB_M_R,  _______,     _______,  _______,  _______,  _______,  RGB_M_P,  RGB_RMOD,  RGB_MOD,  RGB_TOG,
+        _______,  _______,  RGB_SAI,  _______,  _______,  _______,     RGB_HUI,  _______,  _______,  _______,  RGB_SPD,   RGB_SPI,            _______,
+        _______,  KC_SLEP,  _______,  _______,  _______,  RGB_VAI,     _______,  _______,  RGB_VAD,  RGB_VAI,  KC_UP,              _______,
+        _______,  _______,  _______,            _______,  _______,     _______,            _______,  KC_LEFT,  KC_DOWN,  KC_RIGHT,           _______
+    ),
+
+    [_FN3] = LAYOUT_all(
+        KC_GRV,     KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,       KC_F6,    KC_F7,       KC_F8,     KC_F9,      KC_F10,      KC_F11,    KC_F12,          KC_DEL,
+        KC_CAPS,  _______,  _______,   _______,  _______,    _______,     _______,  _______,  KC_PSCR,  KC_SCRL,  KC_PAUS,    KC_UP,  _______,  _______,
+        _______,  _______,  _______,   _______,  _______,    _______,     _______,  _______,  KC_HOME,  KC_PGUP,  KC_LEFT,  KC_RGHT,            _______,
+        _______,  _______,  _______,   _______,  _______,    _______,     _______,  _______,   KC_END,  KC_PGDN,  KC_DOWN,            _______,
+        _______,  _______,  _______,             _______,    _______,     _______,            _______,  _______,  _______,  _______,           _______
+    )
+};
+
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+    [_BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [_FN1]  = { ENCODER_CCW_CW(RGB_HUD, RGB_HUI) },
+    [_FN2]  = { ENCODER_CCW_CW(RGB_HUD, RGB_HUI) },
+    [_FN3]  = { ENCODER_CCW_CW(RGB_HUD, RGB_HUI) }
+};
+#endif
+
--- keyboards/skyloong/gk61/pro_48/original files/pro_48.c
+++ keyboards/skyloong/gk61/pro_48/original files/pro_48.c
@@ -1,10 +1,10 @@
 // Copyright 2023 linlin012 (@linlin012)
 // SPDX-License-Identifier: GPL-2.0-or-later
 #include "quantum.h"
-_Bool L_BASE = 0;
-_Bool FN_1 = 0;
-_Bool FN_2 = 0;
-_Bool FN_3 = 0;
+_Bool FN_WIN = 0;
+_Bool FN_MAC = 0;
+_Bool L_WIN = 0;
+_Bool L_MAC = 0;
 
 #if defined(RGB_MATRIX_ENABLE)  /*&& defined(CAPS_LOCK_INDEX)*/
 const is31_led PROGMEM g_is31_leds[RGB_MATRIX_LED_COUNT] = {
@@ -89,47 +89,60 @@ bool rgb_matrix_indicators_advanced_kb(uint8_t led_min, uint8_t led_max) {
         return false;
     }
 
-    // Caps Lock LED indicator
     if (host_keyboard_led_state().caps_lock) {
         RGB_MATRIX_INDICATOR_SET_COLOR(CAPS_LOCK_INDEX, 255, 255, 255);
-    } 
-    else {
+    } else {
         if (!rgb_matrix_get_flags()) {
             RGB_MATRIX_INDICATOR_SET_COLOR(CAPS_LOCK_INDEX, 0, 0, 0);
         }
     }
 
-    switch (get_highest_layer(layer_state)) {
-        case 1:{
-            RGB_MATRIX_INDICATOR_SET_COLOR(FN1_MOD_INDEX, 255, 255, 255);
-            if (!rgb_matrix_get_flags()) {
-                RGB_MATRIX_INDICATOR_SET_COLOR(FN2_MOD_INDEX, 0, 0, 0);
-                RGB_MATRIX_INDICATOR_SET_COLOR(FN3_MOD_INDEX, 0, 0, 0);
-            }
-        } break;
-        case 2:{
-            RGB_MATRIX_INDICATOR_SET_COLOR(FN2_MOD_INDEX, 255, 255, 255);
-            if (!rgb_matrix_get_flags()) {
-                RGB_MATRIX_INDICATOR_SET_COLOR(FN1_MOD_INDEX, 0, 0, 0);
-                RGB_MATRIX_INDICATOR_SET_COLOR(FN3_MOD_INDEX, 0, 0, 0);
-            }
-        } break;
-        case 3:{
-            RGB_MATRIX_INDICATOR_SET_COLOR(FN3_MOD_INDEX, 255, 255, 255);
-            if (!rgb_matrix_get_flags()) {
-                RGB_MATRIX_INDICATOR_SET_COLOR(FN1_MOD_INDEX, 0, 0, 0);
-                RGB_MATRIX_INDICATOR_SET_COLOR(FN2_MOD_INDEX, 0, 0, 0);
-             }
-        } break;
+   switch (get_highest_layer(layer_state)) {
+      case 2:{
+        RGB_MATRIX_INDICATOR_SET_COLOR(WIN_MOD_INDEX, 255, 255, 255);
+        if (!rgb_matrix_get_flags()) {
+            RGB_MATRIX_INDICATOR_SET_COLOR(MAC_MOD_INDEX, 0, 0, 0);
+         }
+      } break;
+      case 3:{
+        RGB_MATRIX_INDICATOR_SET_COLOR(MAC_MOD_INDEX, 255, 255, 255);
+        if (!rgb_matrix_get_flags()) {
+            RGB_MATRIX_INDICATOR_SET_COLOR(WIN_MOD_INDEX, 0, 0, 0);
+         }
+      } break;
 
-        // case 0/default: fall through to unlight the lit indicators when returning to base layer
-        default:{
+      case 0:{
+       if (L_WIN) {
+            RGB_MATRIX_INDICATOR_SET_COLOR(WIN_MOD_INDEX, 255, 255, 255);
             if (!rgb_matrix_get_flags()) {
-                RGB_MATRIX_INDICATOR_SET_COLOR(FN1_MOD_INDEX, 0, 0, 0);
-                RGB_MATRIX_INDICATOR_SET_COLOR(FN2_MOD_INDEX, 0, 0, 0);
-                RGB_MATRIX_INDICATOR_SET_COLOR(FN3_MOD_INDEX, 0, 0, 0);
+               RGB_MATRIX_INDICATOR_SET_COLOR(MAC_MOD_INDEX, 0, 0, 0);
             }
-        }
+            }else{
+                if (!rgb_matrix_get_flags()) {
+                   RGB_MATRIX_INDICATOR_SET_COLOR(WIN_MOD_INDEX, 0, 0, 0);
+                 }
+              }
+         } break;
+
+     case 1:{
+       if (L_MAC) {
+            RGB_MATRIX_INDICATOR_SET_COLOR(MAC_MOD_INDEX, 255, 255, 255);
+            if (!rgb_matrix_get_flags()) {
+               RGB_MATRIX_INDICATOR_SET_COLOR(WIN_MOD_INDEX, 0, 0, 0);
+            }
+            }else{
+                if (!rgb_matrix_get_flags()) {
+                   RGB_MATRIX_INDICATOR_SET_COLOR(MAC_MOD_INDEX, 0, 0, 0);
+                 }
+              }
+         } break;
+
+      default:{
+         if (!rgb_matrix_get_flags()) {
+            RGB_MATRIX_INDICATOR_SET_COLOR(WIN_MOD_INDEX, 0, 0, 0);
+            RGB_MATRIX_INDICATOR_SET_COLOR(MAC_MOD_INDEX, 0, 0, 0);
+         }
+      }
     }
     return false;
 }
@@ -166,28 +179,37 @@ bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
             }
          return false;
 #    endif
-
-     case MO(1):
+     case TO(0):
       if (record->event.pressed) {
-       FN_1 = 1;
+       L_WIN = 1;
+       set_single_persistent_default_layer(0); // Save default layer 0 to eeprom
       } else {
-       FN_1 = 0;
+       L_WIN = 0;
       }
       return true; // continue all further processing of this key
 
      case MO(2):
       if (record->event.pressed) {
-       FN_2 = 1;
+       FN_WIN = 1;
       } else {
-       FN_2 = 0;
+       FN_WIN = 0;
+      }
+      return true; // continue all further processing of this key
+
+     case TO(1):
+      if (record->event.pressed) {
+       L_MAC = 1;
+       set_single_persistent_default_layer(1);  //Save default layer 1 to eeprom
+      } else {
+       L_MAC = 0;
       }
       return true; // continue all further processing of this key
 
      case MO(3):
       if (record->event.pressed) {
-       FN_3 = 1;
+       FN_MAC = 1;
       } else {
-       FN_3 = 0;
+       FN_MAC = 0;
       }
       return true; // continue all further processing of this key
     default:
