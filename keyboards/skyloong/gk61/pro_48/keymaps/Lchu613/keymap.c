c12d0c3396d04b789ddfd259f69b784a1e79393f
Landon Chu <lchu059@gmail.com>
2024-03-31 14:01:57 -0400

    Enter on FN1 layer

--- keyboards/skyloong/gk61/pro_48/keymaps/Lchu613/keymap.c
+++ keyboards/skyloong/gk61/pro_48/keymaps/Lchu613/keymap.c
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      [_FN1] = LAYOUT_all(
         KC_GRV,     KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,       KC_F6,    KC_F7,       KC_F8,     KC_F9,      KC_F10,      KC_F11,    KC_F12,          KC_DEL,
         KC_CAPS,  KC_BTN3,  KC_HOME,  KC_MS_U,   KC_END,  KC_BTN1,     KC_PGUP,  KC_HOME,      KC_INS,    KC_END,      KC_DEL,     KC_BSPC,   _______,         _______,
-        _______,  KC_BTN4,  KC_MS_L,  KC_MS_D,  KC_MS_R,  KC_BTN2,     KC_LEFT,  KC_DOWN,       KC_UP,   KC_RGHT,     _______,     _______,                    _______,
+        _______,  KC_BTN4,  KC_MS_L,  KC_MS_D,  KC_MS_R,  KC_BTN2,     KC_LEFT,  KC_DOWN,       KC_UP,   KC_RGHT,      KC_ENT,     _______,                    _______,
         _______,  KC_BTN5,  KC_MPRV,  KC_MPLY,  KC_MNXT,  _______,     KC_PGDN,  KC_VOLD,     KC_VOLU,   KC_MUTE,     _______,                _______,
         _______,  _______,  _______,            _______,  _______,     KC_MPLY,               _______,   _______,     _______,     _______,              MO(_FNX)
     ),
