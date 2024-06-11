b25ef8a117ad137eb33c1a537cfc729166927394
Landon Chu <lchu059@gmail.com>
2024-06-11 02:42:31 -0400

    Right shift key is FN3 when pressed and held, RShift when double pressed and held

--- keyboards/skyloong/gk61/pro_48/keymaps/Lchu613/keymap.c
+++ keyboards/skyloong/gk61/pro_48/keymaps/Lchu613/keymap.c
@@ -12,6 +12,32 @@ enum layer_names {
     _TM
 };
 
+// Tap Dance Declarations
+typedef enum {
+    TD_NONE,
+    TD_UNKNOWN,
+    TD_SINGLE_TAP,
+    TD_SINGLE_HOLD,
+    TD_DOUBLE_TAP,
+    TD_DOUBLE_HOLD
+} td_state_t;
+
+typedef struct {
+    bool is_press_action;
+    td_state_t state;
+} td_tap_t;
+
+enum {
+    TD_RSFT_FN3, // Our custom tap dance key
+};
+
+// Function associated with all tap dances
+td_state_t cur_dance(tap_dance_state_t *state);
+
+// Functions associated with individual tap dances
+void ql_finished(tap_dance_state_t *state, void *user_data);
+void ql_reset(tap_dance_state_t *state, void *user_data);
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
      * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
@@ -30,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          KC_ESC,     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,        KC_6,     KC_7,        KC_8,      KC_9,        KC_0,     KC_MINS,    KC_EQL,         KC_BSPC,
          KC_TAB,     KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,        KC_Y,     KC_U,        KC_I,      KC_O,        KC_P,     KC_LBRC,   KC_RBRC,         KC_BSLS,
         KC_LCTL,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,        KC_H,     KC_J,        KC_K,      KC_L,     KC_SCLN,     KC_QUOT,                     KC_ENT,
-        KC_LSFT,     KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,        KC_N,     KC_M,     KC_COMM,    KC_DOT,     KC_SLSH,                MO(_FN3),
+        KC_LSFT,     KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,        KC_N,     KC_M,     KC_COMM,    KC_DOT,     KC_SLSH,                         TD(TD_RSFT_FN3),
         KC_LCTL,  KC_LGUI,  KC_LALT,             KC_SPC,   KC_SPC,     KC_MUTE,              MO(_FN1),   KC_RALT,    MO(_FN3),     KC_RCTL,              MO(_FN2)
     ),
 
@@ -74,6 +100,69 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  _______,  _______,             _______,    _______,     _______,            _______,  _______,  _______,  _______,            _______
     ),
 };
+
+// Determine the current tap dance state
+td_state_t cur_dance(tap_dance_state_t *state) {
+    if (state->count == 1) {
+        if (!state->pressed) return TD_SINGLE_TAP;
+        else return TD_SINGLE_HOLD;
+    } else if (state->count == 2) {
+        if (!state->pressed) return TD_DOUBLE_TAP;
+        else return TD_DOUBLE_HOLD;
+    }
+    else return TD_UNKNOWN;
+}
+
+// Initialize tap structure associated with example tap dance key
+static td_tap_t ql_tap_state = {
+    .is_press_action = true,
+    .state = TD_NONE
+};
+
+// Functions that control what our tap dance key does
+void ql_finished(tap_dance_state_t *state, void *user_data) {
+    ql_tap_state.state = cur_dance(state);
+    switch (ql_tap_state.state) {
+        case TD_SINGLE_HOLD:
+            layer_on(_FN3);
+            break;
+        case TD_DOUBLE_HOLD:
+            register_code(KC_RSFT);
+            break;
+        default:
+            break;
+    }
+}
+
+void ql_reset(tap_dance_state_t *state, void *user_data) {
+    // If the key was held down and now is released then switch off the layer
+    switch (ql_tap_state.state) {
+        case TD_SINGLE_HOLD:
+            layer_off(_FN3);
+            break;
+        case TD_DOUBLE_HOLD:
+            unregister_code(KC_RSFT);
+            break;
+        default:
+            break;
+    }
+    ql_tap_state.state = TD_NONE;
+}
+
+// Associate our tap dance key with its functionality
+tap_dance_action_t tap_dance_actions[] = {
+    [TD_RSFT_FN3] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, ql_finished, ql_reset)
+};
+
+// Set a long-ish tapping term for tap-dance keys
+//uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+//    switch (keycode) {
+//        case QK_TAP_DANCE ... QK_TAP_DANCE_MAX:
+//            return 275;
+//        default:
+//            return TAPPING_TERM;
+//    }
+//}
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
